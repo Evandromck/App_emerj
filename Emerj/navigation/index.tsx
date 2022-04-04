cff1d053d6ddfac7dc1a96a24ae407a82359a5d4
Evandro Araujo <64655153+Evandromck@users.noreply.github.com>
2022-04-04 13:31:49 -0300

    Deploy teste tela

--- Emerj/navigation/index.tsx
+++ Emerj/navigation/index.tsx
@@ -12,10 +12,13 @@ import { ColorSchemeName, Pressable } from 'react-native';
 
 import Colors from '../constants/Colors';
 import useColorScheme from '../hooks/useColorScheme';
+
 import ModalScreen from '../screens/ModalScreen';
 import NotFoundScreen from '../screens/NotFoundScreen';
 import TabOneScreen from '../screens/TabOneScreen';
 import TabTwoScreen from '../screens/TabTwoScreen';
+import UserScreen from '../screens/UserScreen';
+
 import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../types';
 import LinkingConfiguration from './LinkingConfiguration';
 
@@ -92,6 +95,17 @@ function BottomTabNavigator() {
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
+      <BottomTab.Screen
+        name="User"
+        component={UserScreen}
+        options={{
+          title: 'Usuário',
+          tabBarIcon: ({ color }) => <TabBarIcon name="user" color={color} />,
+        }}
+      />
+
+
+
     </BottomTab.Navigator>
   );
 }
